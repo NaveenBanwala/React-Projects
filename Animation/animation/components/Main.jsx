2f8cf62435e896ac9498cf7deb9d897041beb9e3
NaveenBanwala <nbanwala7@gmail.com>
2025-05-25 19:09:33 +0530

    changes in few components

---
+++ Animation/animation/components/Main.jsx
@@ -0,0 +1,15 @@
+function Main(){
+    return (<>
+    <main>
+                <h1>This is default text</h1>
+                <div>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt officiis cupiditate quod. Possimus ex ducimus quos quae, praesentium tempora eveniet aut libero, pariatur veritatis perferendis voluptatibus ipsum, dignissimos dolorum vel!
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus ea, ipsa, autem saepe facilis quam minima qui voluptates quia dolores optio, perferendis vel dignissimos ullam! Quod nam quos veritatis eos.
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati quod, non eligendi esse iure voluptatum nam magni! Numquam, eos, dolorum porro, iure explicabo sequi in quisquam animi unde optio magnam.
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint quam culpa in consequuntur, quisquam est sapiente accusantium repellendus aut officiis temporibus molestiae quo velit, delectus sunt quibusdam ipsum, vitae vel?
+                </div>
+            </main>
+    </>);
+
+}
+export default Main;
--- Animation/animation/src/App.jsx
+++ Animation/animation/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import {useEffect,useState} from "react";
 import Header from "../components/Header";
 import Section from "../components/Section";
+import Main from "../components/Main";
 
 function App(){
     const [inital,setNext] = useState(0);
@@ -13,22 +14,11 @@ function App(){
     window.addEventListener("scroll",handleScroll);
 
 },[])
-
-
     return(
         <>
         <Header/>
         <Section inital={inital}/>
-
-            <main>
-                <h1>This is default text</h1>
-                <div>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt officiis cupiditate quod. Possimus ex ducimus quos quae, praesentium tempora eveniet aut libero, pariatur veritatis perferendis voluptatibus ipsum, dignissimos dolorum vel!
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus ea, ipsa, autem saepe facilis quam minima qui voluptates quia dolores optio, perferendis vel dignissimos ullam! Quod nam quos veritatis eos.
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati quod, non eligendi esse iure voluptatum nam magni! Numquam, eos, dolorum porro, iure explicabo sequi in quisquam animi unde optio magnam.
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint quam culpa in consequuntur, quisquam est sapiente accusantium repellendus aut officiis temporibus molestiae quo velit, delectus sunt quibusdam ipsum, vitae vel?
-                </div>
-            </main>
+        <Main/>
             </>
     );
 }
